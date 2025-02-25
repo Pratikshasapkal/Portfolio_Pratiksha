0efa58603747e98003868db569202e852d1d8280
Pratikshasapkal <130816927+Pratikshasapkal@users.noreply.github.com>
2025-02-25 22:33:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+        // Toggle Menu Function
+        function toggleMenu() {
+            const navLinks = document.querySelector('.header.nav-links');
+            navLinks.classList.toggle('active');
+        }
